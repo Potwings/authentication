46981289280a6987abc185963ee4d6fbf7210fe0
Potwings <ygk0719@gmail.com>
2025-09-06 22:12:38 +0900

    feat : Spring Security 초기 환경 생성(form을 활용한 로그인)

--- src/main/java/org/potwings/authentication/config/SecurityConfig.java
+++ src/main/java/org/potwings/authentication/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package org.potwings.authentication.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(auth -> auth
+            .anyRequest().authenticated()
+        )
+        .formLogin(form -> form.defaultSuccessUrl("/home", true));
+
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+
+    UserDetails user = User.withUsername("test")
+        .password(passwordEncoder().encode("test")) // PasswordEncoder 미사용 시 비밀번호로 사용 불가
+        .roles("test")
+        .build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
--- src/main/java/org/potwings/authentication/controller/HomeController.java
+++ src/main/java/org/potwings/authentication/controller/HomeController.java
@@ -1,4 +1,17 @@
 package org.potwings.authentication.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
 public class HomeController {
+
+  @GetMapping("/home")
+  public void getHome() {
+    log.info("Hello Home!");
+    log.info("Hello Home!");
+    log.info("Hello Home!");
+  }
 }
